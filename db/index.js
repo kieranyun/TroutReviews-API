764185257d0157c2e3073b6f4bd556f8718ada95
Kieran <kieran.yun@gmail.com>
2022-04-27 18:36:43 -0700

    updated env

--- db/index.js
+++ db/index.js
@@ -3,8 +3,8 @@ const { Pool } = require('pg');
 const pool = new Pool({
   user: process.env.USER,
   host: process.env.HOST || 'localhost',
-  database: process.env.db || 'reviews',
-  port: process.env.PORT,
+  database: process.env.DB || 'reviews',
+  port: process.env.DBPORT,
 });
 
 module.exports = {
