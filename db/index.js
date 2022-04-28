ec766fbbb410b24210eddb576147679302e897d9
Kieran <kieran.yun@gmail.com>
2022-04-27 22:38:15 -0700

    fixed more dotenv issues

--- db/index.js
+++ db/index.js
@@ -1,12 +1,17 @@
 const { Pool } = require('pg');
+require('dotenv').config();
 
 const pool = new Pool({
-  user: process.env.USER,
+  user: process.env.U,
   host: process.env.HOST || 'localhost',
   database: process.env.DB || 'reviews',
+  password: process.env.PASSWORD,
   port: process.env.DBPORT,
 });
 
 module.exports = {
-  query: (query, params) => pool.query(query, params),
+  query: (query, params) => {
+    console.log(pool);
+    return pool.query(query, params);
+  },
 };
