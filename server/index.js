53aba818ba2e72449b33d5631390917ceda93e9f
Kieran <kieran.yun@gmail.com>
2022-04-27 18:23:10 -0700

    set up env

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@
 /* eslint-disable no-console */
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 1337;
